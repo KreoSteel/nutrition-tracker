ce53d419cd0c56905830d286449e5eec4e15a684
KreoSteel <tatarchukluca@gmail.com>
2025-10-09 01:06:04 +0200

    Add dependencies for Prisma, Supabase, and React Query; add database schemas.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
+import { prisma } from "../../utils/prisma/prisma";
 
-export default function Home() {
+export default async function Home() {
+
+  const ingredients = await prisma.ingredient.findMany();
+
+  console.log(ingredients);
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
